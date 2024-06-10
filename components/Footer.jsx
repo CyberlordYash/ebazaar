f078bc8d28e7ac1f8f60f7bfdc444fea736eefb9
Yash Sachan <yashsachan321@gmail.com>
2024-06-10 23:16:30 +0530

    minor ui improvements

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,6 +8,12 @@ const Footer = () => {
         <AiFillInstagram></AiFillInstagram>
         <AiFillTwitterCircle></AiFillTwitterCircle>
       </p>
+      <div className=" footer-portfolio">
+        Check out my other projects at{" "}
+        <a href="https://yashsachan.netlify.app/">
+          <button className="footer-button">Portfolio</button>
+        </a>
+      </div>
     </div>
   );
 };
